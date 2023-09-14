1dcb69cc75630c7e0ad7fba85b49bc3f19ac1399
Samuel Martins <samuelsilvaa431@gmail.com>
2023-09-14 17:02:19 -0300

    Terminando ex3

--- ex01/script.js
+++ ex01/script.js
@@ -3,7 +3,7 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     //var hora = data.getHours()
-    var hora = 1    
+    var hora = 10
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora >= 0 && hora < 12){
         img.src = 'Fotos/manhã-300.jpg'
--- ex03/script.js
+++ ex03/script.js
@@ -1,18 +1,18 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    //var hora = data.getHours()
-    var hora = 1    
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if(hora >= 0 && hora < 12){
-        img.src = 'Fotos/manhã-300.jpg'
-        document.body.style.background = '#F98B36'
-    } else if(hora >= 12 && hora < 18){
-        img.src = 'Fotos/tarde-300.jpg'
-        document.body.style.background = '#221D3D'
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor digite um numero')
     } else {
-        img.src = 'Fotos/noite-300.jpg'
-        document.body.style.background = '#132029'
+        var n = Number(num.value)
+        tab.innerHTML = ''
+        for(var c = 1; c <= 10; c++){
+            var item = document.createElement('option')
+            item.value = `tab${c}`
+            item.text = `${n} x ${c} = ${ n * c}`       
+            tab.appendChild(item)                       
+        }
     }
+
 }
