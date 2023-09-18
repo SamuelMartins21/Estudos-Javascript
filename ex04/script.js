b926967f5d0cedb8e9c094dd0bdb1856f72789d1
Samuel Martins <samuelsilvaa431@gmail.com>
2023-09-18 17:34:38 -0300

    Atualizando projeto

--- ex04/script.js
+++ ex04/script.js
@@ -1,31 +1,64 @@
-function adicionar() {
-    var num = document.getElementById('txtn')
-    var nums = document.getElementById('selnums')
-   
+var num = document.getElementById('txtn')
+var lista = document.getElementById('selnums')
+var result = document.getElementById('res')
+var valores = []
 
-    if (num.value.length == 0) {
-        window.alert('Por favor digite um numero')
-    } else {
-        var n = []
-        n.push(Number(num.value))
-        var item = document.createElement('option')
-        item.text = ` Você adicionou o número ${n}`
-        nums.appendChild(item)
-        return n
+function isNumero(n){
+    if(Number(n) >=1 && Number(n) <= 100){
+        return true
+    } else{
+        return false
     }
-
 }
 
-function finalizar() {
-    var num = document.getElementById('txtn')
-    var nums = document.getElementById('selnums')
-    var result = document.getElementById('resultado')
-
-    if (num.value.length == 0) {
-        window.alert('Por favor digite um numero')
-    } else {
-        var ad = adicionar()
-        result.innerHTML = `você digitou ` + ad
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    } else{
+        return false
     }
+}
 
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+
+    } else{
+        window.alert('[ERROR] Digite um numero valido ou que ainda não tenha sido adicionado')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores na lista')
+    } else{
+        var tot = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+
+        for(var pos in valores){
+            if(valores[pos] > maior){
+                soma += valores[pos]
+                media = soma/tot
+                maior = valores[pos]
+            } else if (valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+
+        result.innerHTML = ''
+        result.innerHTML += `<p>A lista tem ${tot} valores<p/>`
+        result.innerHTML += `<p>O maior numero é ${maior}<p/>`
+        result.innerHTML += `<p>O menor numero é ${menor}<p/>`
+        result.innerHTML += `<p>A soma de todos os valores é ${soma}<p/>`
+        result.innerHTML += `<p>A média de todos os valores é ${media}<p/>`
+        
+    }
 }
