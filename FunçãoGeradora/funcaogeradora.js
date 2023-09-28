da09f32a2b980b50f9e746373a151bc5680370c1
Samuel Martins <samuelsilvaa431@gmail.com>
2023-09-28 14:36:40 -0300

    Funções Geradoras

--- FunçãoGeradora/funcaogeradora.js
+++ FunçãoGeradora/funcaogeradora.js
@@ -10,3 +10,15 @@ function* minhaFuncaoGeradora() {
   console.log(gerador.next()); 
   console.log(gerador.next()); 
   console.log(gerador.next());
+
+function* perguntas() {
+    const nome = yield 'Qual é o seu nome?'
+    const esporte = yield 'Qual é o seu esporte favorito?'
+    return "Seu nome é " + nome + " seu esporte favorito é " + esporte
+}
+
+const teste = perguntas()
+
+console.log(teste.next().value);
+console.log(teste.next('Samuel').value);
+console.log(teste.next('Natação').value);
